fc7053ee3582a1fa563ca98af51f163fc7128dfe
Agus Moles <agusmoles@gmail.com>
2023-03-06 20:12:02 -0300

    Observe elements recursively (#3)

--- hide-ml-address.js
+++ hide-ml-address.js
@@ -1,17 +1,67 @@
-const observer = new MutationObserver((mutations) => {
-  mutations.forEach((mutation) => {
-    mutation.addedNodes.forEach((node) => {
-      if (
-        typeof node.className === "string" &&
-        node.className.includes("nav-menu-link-cp")
-      ) {
-        node.textContent = "***************";
-      }
+const clearTextContent = (node) => (node.textContent = "***************");
+
+const nodeClassNameContains = (node, className) =>
+  node.nodeType === Node.ELEMENT_NODE &&
+  typeof node.className === "string" &&
+  node.className.includes(className);
+
+const isMLChangeAddressButton = (node) =>
+  nodeClassNameContains(node, "nav-menu-cp nav-menu-cp-logged");
+
+const clearNavbarMLAddress = (node) => {
+  if (nodeClassNameContains(node, "nav-menu-link-cp")) clearTextContent(node);
+};
+
+const clearInnerAddresses = (node) => {
+  const innerAddresses = node.querySelectorAll?.(
+    '[class*="card-block__text--addresses"]'
+  );
+  if (innerAddresses) {
+    innerAddresses.forEach((addressNode) => {
+      clearTextContent(addressNode);
+    });
+  }
+};
+
+const clearPersonalDataProfile = (node) => {
+  const personalDataProfiles = node.querySelectorAll?.(
+    '[class*="main-container profile"]'
+  );
+
+  if (personalDataProfiles) {
+    personalDataProfiles.forEach((profile) => {
+      clearTextContent(profile);
+    });
+  }
+};
+
+const observeElement = (document) => {
+  const observer = new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
+      mutation.addedNodes.forEach((node) => {
+        clearNavbarMLAddress(node);
+        clearInnerAddresses(node);
+        clearPersonalDataProfile(node);
+
+        if (isMLChangeAddressButton(node)) {
+          const changeAddressButton = node;
+
+          changeAddressButton.href = "javascript:void(0)";
+          changeAddressButton.addEventListener("click", (e) => {
+            e.stopPropagation();
+            alert(
+              "No puedes cambiar tu dirección de envío, desactiva la extension de proteccion de direccion de Mercado Libre"
+            );
+          });
+        }
+      });
     });
   });
-});
 
-observer.observe(document, {
-  childList: true, // watch for changes to the child nodes
-  subtree: true, // watch for changes in all descendants of the body
-});
+  observer.observe(document, {
+    childList: true, // watch for changes to the child nodes
+    subtree: true, // watch for changes in all descendants of the body
+  });
+};
+
+observeElement(document);
