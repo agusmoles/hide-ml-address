9ec63180b697cc96ad3fec09b2db6d1ef1d5c79f
Agus Moles <agusmoles@gmail.com>
2023-03-06 23:27:24 -0300

    Dinamically updates node contents regarding switch status

--- background.js
+++ background.js
@@ -1,11 +1,21 @@
+const sendTabsMessage = (message) => {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    try {
+      chrome.tabs.sendMessage(tabs[0].id, message);
+    } catch (e) {
+      console.log(e);
+    }
+  });
+};
+
 const setStorage = (value) => {
   chrome.storage.sync.set({ "hide-ml-address": value }, () => {
     console.log("Set hide ML Address storage to ", value);
   });
+  sendTabsMessage(value === "true" ? "hide-ml-address" : "show-ml-address");
 };
 
 chrome.runtime.onInstalled.addListener(() => {
-  console.log("asd installed");
   chrome.runtime.onMessage.addListener(function (request, _, sendResponse) {
     if (request === "hide-ml-address") setStorage("true");
     if (request === "show-ml-address") setStorage("false");
