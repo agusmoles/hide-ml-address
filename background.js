400be7f53c7a28ed038dc581bf82838daafd7326
Agus Moles <agusmoles@gmail.com>
2023-03-06 22:08:22 -0300

    Adds toggle button to disable the extension (#2)
    * Adds toggle button to disable the extension
    
    * Adds background and content script and communication

---
+++ background.js
@@ -0,0 +1,26 @@
+const setStorage = (value) => {
+  chrome.storage.sync.set({ "hide-ml-address": value }, () => {
+    console.log("Set hide ML Address storage to ", value);
+  });
+};
+
+chrome.runtime.onInstalled.addListener(() => {
+  console.log("asd installed");
+  chrome.runtime.onMessage.addListener(function (request, _, sendResponse) {
+    if (request === "hide-ml-address") setStorage("true");
+    if (request === "show-ml-address") setStorage("false");
+    if (request === "get-hide-ml-address") {
+      chrome.storage.sync.get("hide-ml-address", function (items) {
+        const response = items["hide-ml-address"] || "true";
+        if (chrome.runtime.lastError) {
+          console.error(chrome.runtime.lastError);
+          return;
+        }
+        sendResponse(response);
+      });
+
+      return true;
+    }
+    return false;
+  });
+});
---
+++ toggle.js
@@ -0,0 +1,17 @@
+const sendMessage = (message, callback) => {
+  if (callback) chrome.runtime.sendMessage(message, callback);
+  else chrome.runtime.sendMessage(message);
+};
+
+const checkbox = document.querySelector('input[class="checkbox"]');
+
+checkbox.addEventListener("change", function (e) {
+  const isChecked = e.target.checked;
+  if (isChecked) sendMessage("hide-ml-address");
+  else sendMessage("show-ml-address");
+});
+
+sendMessage("get-hide-ml-address", function (response) {
+  if (response === "true") checkbox.checked = true;
+  else checkbox.checked = false;
+});
